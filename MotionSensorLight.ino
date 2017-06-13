0fa3e897230b99f8fc3da953bc23b92f19264aa0
Rohan Ahmad <rohanoahmad@gmail.com>
2017-06-13 11:55:51 +0800

    Added Kahman's code

---
+++ MotionSensorLight.ino
@@ -0,0 +1,26 @@
+void setup() {
+  // put your setup code here, to run once:
+
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(2,INPUT); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int motion = digitalRead(2);
+
+  if (motion == 1) {
+    //analogWrite(pin,number) (5,255)
+  digitalWrite(5, HIGH); 
+  digitalWrite(6, HIGH);
+  digitalWrite(9, HIGH);
+
+  } else {
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(9,LOW);
+  }
+
+}
